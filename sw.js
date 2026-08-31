f11a842e882314479097a70ba9bfb8b1c6decbed
Elizarcthulhu99 <onorato.pietro91@gmail.com>
2026-08-31 13:04:54 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'sessioni-gdr-v1';
+const FILES_TO_CACHE = ['./index.html', './manifest.json', './icon.svg'];
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
+});
